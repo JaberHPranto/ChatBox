cecafdcdd4bd89c366c609860401df064c26c69f
JaberHPranto <jhpranto53@gmail.com>
2021-08-10 17:31:47 +0600

    add login with google and facebook feature

---
+++ src/components/Login.js
@@ -0,0 +1,23 @@
+import { FacebookFilled, GoogleOutlined } from '@ant-design/icons'
+import "firebase/app"
+import firebase from 'firebase/app'
+import React from 'react'
+import { auth } from '../firebase'
+
+
+function Login() {
+    return (
+        <div id="login-page">
+            <div id="login-card">
+                <h2>Welcome to Chat Box</h2>
+                <div className="login-button google" onClick={()=> auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}><GoogleOutlined />
+                Sign in with Google</div>
+                <br/><br/>
+                <div className="login-button facebook" onClick={()=> auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}><FacebookFilled/>
+                Sign in with Facebook</div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
